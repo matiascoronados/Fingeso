25fdf2b131fda156829c8552003a1aa471116675
Matias Coronado <matias.coronado@usach.cl>
2019-01-10 01:38:18 -0300

    Creacion proyecto

---
+++ src/main/java/com/fingeso/reddeideas/ReddeideasApplication.java
@@ -0,0 +1,14 @@
+package com.fingeso.reddeideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddeideasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReddeideasApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/fingeso/reddeideas/ReddeideasApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fingeso.reddeideas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReddeideasApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
